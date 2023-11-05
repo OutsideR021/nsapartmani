f9ea98a09731cd2060f01031d53a05498d8c8360
DESKTOP-8E5DMBC\OutsideR <outsider21pc@gmail.com>
2023-11-05 13:26:31 +0100

    first commit

---
+++ wp-content/plugins/essential-blocks/lib/global-styles/dist/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => 'ff63f7cb8959c280d0b3');
---
+++ wp-content/plugins/templately/assets/js/elementor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '88f1213806d196b91376');
